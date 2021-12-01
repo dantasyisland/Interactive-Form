79a0b891ce4f4b6bea42eeb1b9dacae9998ad4b0
dantasyisland <dantothelastdrop@yahoo.com>
2021-12-01 17:16:52 -0600

    Added more variables

--- js/formplay.js
+++ js/formplay.js
@@ -3,28 +3,51 @@
 
 //BASIC INFO fieldset
 
-let name = document.getElementById('name'); // input
-let email = document.getElementById('email'); // input
-let jobRole = document.getElementById('title'); // select - 1st option has hidden attribute - last option is Other - options have values
-let otherJobRole = document.getElementById('other-job-role'); // input - other job role
+const name = document.getElementById('name'); // input
+const email = document.getElementById('email'); // input
+const jobRole = document.getElementById('title'); // select - 1st option has hidden attribute - last option is Other - options have values
+const otherJobRole = document.getElementById('other-job-role'); // input - other job role
 
 //SHIRTS fieldset
-let tShirtSize = document.getElementById('size'); // select - options have values
-let tShirtDesign = document.getElementById('design'); // select - options have values
+const tShirtSize = document.getElementById('size'); // select - options have values
+const tShirtDesign = document.getElementById('design'); // select - options have values
 
-let tShirtDesignOptions = document.querySelectorAll('#design option') // SMARTER WAY TO GO
-let tShirtColor = document.getElementById('color'); // select - options have data-theme js puns or heart js
+const tShirtDesignOptions = document.querySelectorAll('#design option') // SMARTER WAY TO GO
+const tShirtColor = document.getElementById('color'); // select - options have data-theme js puns or heart js
 
 //ACTIVITES fieldset
-let activitiesBox = document.getElementById('activities-box');
+const activitiesBox = document.getElementById('activities-box');
 
 
-// let checkboxes = activitiesBox.children; // HTML collection of labels - input type checkbox
-let activitiesCheckboxes = document.querySelectorAll('.activities input'); // node list  HTML collection of inputs type - checkbox
+// const checkboxes = activitiesBox.children; // HTML collection of labels - input type checkbox
+const activitiesCheckboxes = document.querySelectorAll('.activities input'); // node list  HTML collection of inputs type - checkbox
 
 
 // Payment Info
-let payment = document.getElementById('payment');
-let payment2 = document.querySelectorAll('#payment option'); // values - select method hidden
+const paymentOptions = document.querySelectorAll('#payment option'); // values - select method hidden
 
 
+const creditCard = document.querySelector('#credit-card');
+//Credit card has div creditcard
+
+const creditCardInfo = document.querySelectorAll('#credit-card select')
+
+const expMonth = document.querySelectorAll('#exp-month option'); // values month of year
+const expYear = document.querySelectorAll('#exp-year option'); //
+
+
+// in Num box
+const creditCardNumber = document.querySelector('#cc-num');
+const creditCardZip = document.querySelector('#zip') ///hmmmm
+const cvv = document.querySelector('#cvv')
+
+const paypal = document.querySelector('#paypal');
+const bitcoin = document.querySelector('#bitcoin');
+
+
+const registerButton = document.querySelector('button[type="submit"]')
+
+
+paypal.style.display = "none";
+bitcoin.style.display = "none";
+creditCard.style.display = "none";
